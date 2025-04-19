70024fe743f497a07309bda268d8cebae7028e97
nectorofficial <ikhumwa40@gmail.com>
2025-04-19 16:24:35 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,89 @@
+/*
+  project_name : nector-xmd Bot
+  Version : 2.4.0
+  Owner : nector
+  Do Not Clone 
+
+
+*/
+
+//----------------------[ nector-xmd]----------------------//
+
+const fs = require('fs')
+const { color } = require('./lib/color')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+//--------------------[ SESSION ID ]----------------------//
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+//Enter your nector-xmd session id here; must start with KANGO~
+
+//--------------------[ BOT NAME ]----------------------//
+
+global.botname = process.env.BOT_NAME || 'nector-xmd' 
+
+//-----------------[ OWNER NUMBER ]------------------//
+
+global.ownernumber = process.env.OWNER_NUMBER || '254725474072' 
+
+//-----------------[ OWNER NAME ]------------------//
+
+global.ownername = process.env.OWNER_NAME || 'nector' 
+
+//------------[ STICKER PACKNAME ]-----------------//
+
+global.packname = process.env.STICKER_PACK_NAME || "nector-xmd" 
+
+//--------------[ STICKER AUTHOR NAME ]------------//
+
+global.author = process.env.STICKER_AUTHOR_NAME || "nector" 
+
+//----------------------[ TIMEZONE ]--------------------//
+
+global.timezones = process.env.TIMEZONE || "Africa/Nairobi" 
+//Don't edit this if you don't know!
+
+//----------------[ GITHUB DATABASE ]-----------------//
+
+global.dbToken = process.env.GITHUB_TOKEN || "";
+// Not really necessary on panels/vps/termux, just put it when bot settings reset when bot restarts.
+
+//Go to https://github.com/settings/tokens, select 'Tokens (classic)', then tap 'Generate new token' and select 'Generate new token (classic)'. Enter any note, choose 'No expiration', and under 'Select scopes', tick 'repo'. Scroll down, generate the token, and copy it. Paste it here. If using a single token for multiple bots, change the owner number to avoid settings mixups.
+
+
+//-----------------[ CONTEXT LINK ]--------------------//
+
+global.plink = process.env.PLINK || "https://youtube.com/@nector_official"
+
+//------------------[ WATERMARK ]--------------------//
+
+global.wm = process.env.GL_WM || "> ©nector-xmd"
+
+//---------------------[ REPLIES ]-----------------------//
+
+global.mess = { 
+  done: '*Done*', 
+  success: '> © nector-xmd', 
+  owner: `*You don't have permission to use this command!*`, 
+  group: '*This feature becomes available when you use it in a group!*', 
+  admin: '*You’ll unlock this feature with me as an admin!*', 
+  notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
+}
+
+//--------------[ DEVELOPER SETTINGS ]---------------//
+/* Do not change anything here!!! */
+
+//😞 
+
+//--------------------[ WATCHER ]-----------------------//
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(color(`Updated '${__filename}'`, 'red'))
+  delete require.cache[file]
+  require(file)
+})
+
+//----------------------[ nector-xmd ]----------------------//
