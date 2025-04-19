4ddbed2385f68b699783343fd60ec9b39500b3cc
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-04-19 22:20:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~ol53WDCD#fdGiNjq6kSg4PXFkpLl0m50oLZMJvT2rXC3wbGYH0WM' 
 //Enter your nector-xmd session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
